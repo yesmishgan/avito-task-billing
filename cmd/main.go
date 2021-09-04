7d2b2d524f9f9db3ed41508ccfeb3f128f15832f
Misha Dolbnin <dolbnin.mihail@yandex.ru>
2021-09-04 16:24:13 +0300

    added config

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	//"github.com/yesmishgan/autumn-2021-intern-assignment"
+)
 
 func main() {
+	if err := InitConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	//srv := new()
+	//if err :=
 	fmt.Println("test")
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
